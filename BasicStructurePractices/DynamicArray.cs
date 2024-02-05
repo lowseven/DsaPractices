16ab66a4292136d140727b9fe1a6414082ea8270
Lowseven <railyn.lopez@gmail.com>
2024-02-05 22:43:52 +0100

    Adding DynamicArray basic structure - adding NCrunch setup and test class lib - adding gitignore

---
+++ BasicStructurePractices/DynamicArray.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+
+namespace BasicStructurePractices;
+
+public sealed class DynamicArray<T> : IEnumerable<T>
+{
+    private T[] _array;
+
+    public int Size { get; private set; } = 0;
+    public int Capacity { get; private set; } = 0;
+    public bool IsEmpty => Size == 0;
+
+    public T this[int index] => index < Size ? _array[index]
+        : throw new ArgumentOutOfRangeException($"index out of range {nameof(index)}");
+
+    public DynamicArray() : this(16) { }
+
+    public DynamicArray(int capacity)
+    {
+        if (capacity < 0 && Size < capacity)
+            throw new ArgumentOutOfRangeException($"index out of range {nameof(capacity)}");
+
+        this.Capacity = capacity;
+        this._array = new T[capacity];
+    }
+
+
+    public void Add(T item)
+    {
+        if (this.Size + 1 >= this._array.Length)
+        {
+            this.Capacity = this.Capacity * 2;
+
+            var newArray = new T[this.Capacity];
+            this._array.CopyTo(newArray, 0);
+            this._array = newArray;
+        }
+
+        this._array[this.Size++] = item;
+    }
+
+
+    public T RemoveAt(int index)
+    {
+        if (index < 0 && Size < index)
+            throw new ArgumentOutOfRangeException($"index out of range{nameof(index)}");
+
+        var newArray = new T[this.Capacity];
+        var resVal = default(T);
+        for (int i = 0, j = 0; i < this.Size; i++)
+        {
+            if (i != index)
+            {
+                newArray[j++] = this._array[i];
+            }
+            else
+            {
+                resVal = this._array[i];
+            }
+        }
+
+        this._array = newArray;
+        this.Size--;
+        this.Capacity--;
+
+        return resVal!;
+    }
+
+    public T Remove(T item)
+    {
+        if(item is null)
+            throw new ArgumentNullException(nameof(item));
+
+        var index = 0;
+        foreach (var t in this._array)
+        {
+            if (t!.Equals(item))//using Object.Equals()..
+                break;
+
+            index++;
+        }
+
+        return RemoveAt(index);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        foreach (var item in this._array.Take(this.Size))
+        {
+            yield return item;
+        }
+    }
+
+    public override string ToString() =>
+        $"[{string.Join(", ", this).Trim()}]";
+
+    IEnumerator IEnumerable.GetEnumerator() => this._array.GetEnumerator();
+}
---
+++ BasicStructureTests/DynamicArrayTest.cs
@@ -0,0 +1,144 @@
+using BasicStructurePractices;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace BasicStructureTests;
+
+[TestClass]
+public class DynamicArrayTest
+{
+    [TestMethod]
+    public void DynamicArray_Should_Init_Array()
+    {
+        //ARRANGE
+        var capacity = 20;
+
+        //ACT
+        var uut = new DynamicArray<string>(capacity);
+
+        //ASSERT
+        uut.Capacity.Should().Be(capacity);
+        uut.Size.Should().Be(0);
+        uut.IsEmpty.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void DynamicArray_Add_Should_Be_Able_To_Add_Items()
+    {
+        //ARRANGE
+        var uut = new DynamicArray<string>();
+
+        //ACT
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+
+        //ASSERT
+        uut.Capacity.Should().Be(16);
+        uut.Size.Should().Be(3);
+        uut[0].Should().Be("Hello");
+        uut[1].Should().Be("World");
+        uut[2].Should().Be("!!");
+    }
+
+    [TestMethod]
+    public void DynamicArray_Add_Should_Be_Able_To_Resize_MaximmumCapacity()
+    {
+        //ARRANGE
+        var uut = new DynamicArray<string>(2);
+
+        //ACT
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+        uut.Add("!!");
+
+        //ASSERT
+        uut.Capacity.Should().Be(8);
+        uut.Size.Should().Be(4);
+        uut[0].Should().Be("Hello");
+        uut[1].Should().Be("World");
+        uut[2].Should().Be("!!");
+        uut[3].Should().Be("!!");
+    }
+
+    [TestMethod]
+    public void DynamicArray_RemoveAt_Should_Remove_Item_At_index()
+    {
+        //ARRANGE
+        var uut = new DynamicArray<string>(2);
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+        uut.Add("!!");
+
+        //ACT
+        var removedItem = uut.RemoveAt(1);
+
+        //ASSERT
+        uut.Capacity.Should().Be(7);
+        uut.Size.Should().Be(3);
+        uut[0].Should().Be("Hello");
+        uut[1].Should().Be("!!");
+        uut[2].Should().Be("!!");
+
+        removedItem.Should().Be("World");
+    }
+
+    [TestMethod]
+    public void DynamicArray_Remove_Should_Remove_Item_At_index()
+    {
+        //ARRANGE
+        var uut = new DynamicArray<string>(2);
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+        uut.Add("!!");
+
+        //ACT
+        var removedItem = uut.Remove("!!");
+
+        //ASSERT
+        uut.Capacity.Should().Be(7);
+        uut.Size.Should().Be(3);
+        uut[0].Should().Be("Hello");
+        uut[1].Should().Be("World");
+        uut[2].Should().Be("!!");
+
+        removedItem.Should().Be("!!");
+    }
+
+    [TestMethod]
+    public void DynamicArray_Enumerator_Should_Be_Iterable()
+    {
+        //ARRANGE
+        var uut = new DynamicArray<string>(2);
+
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+        uut.Add("!!");
+
+        //ACT, ASSERT
+        uut.SequenceEqual(new[] { "Hello", "World", "!!", "!!" }).Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void DynamicArray_ToString_Should_Be_List_All_Item_Into_A_FormattedString()
+    {
+        //ARRANGE
+        var expectedVal = $"[{string.Join(", ", new[] { "Hello", "World", "!!", "!!" }).Trim()}]";
+
+        //ACT
+        var uut = new DynamicArray<string>(2);
+
+        uut.Add("Hello");
+        uut.Add("World");
+        uut.Add("!!");
+        uut.Add("!!");
+
+        //ACT
+        expectedVal.ToString().Should().Be(uut.ToString());
+    }
+}
