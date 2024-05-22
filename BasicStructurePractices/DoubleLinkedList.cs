fdb6eb023d65b03f40fded281596ce9a07f617fb
lowseven <Lowseven@live.com>
2024-05-22 10:05:31 -0400

    Adding DoubleLinkedList and tests cases

---
+++ BasicStructurePractices/DoubleLinkedList.cs
@@ -0,0 +1,254 @@
+﻿using System.Collections;
+
+namespace BasicStructurePractices;
+
+public sealed class DoubleLinkedList<T> : IEnumerable<T>
+{
+    private class Node<TT>
+    {
+        public TT? Data { get; set; }
+        public Node<TT>? Next { get; set; }
+        public Node<TT>? Prev { get; set; }
+
+        public Node(TT Item, Node<TT>? next, Node<TT>? prev)
+        {
+            this.Next = next;
+            this.Prev = prev;
+            Data = Item;
+        }
+    }
+
+    private Node<T>? _head;
+    private Node<T>? _tail;
+
+    public int Count { get; private set; }
+    public bool IsEmpty => Count is 0;
+
+    public void Add(T item) =>
+        this.AddLast(item);
+
+
+    public void Clear()
+    {
+        var node = _head;
+        while (node is not null)
+        {
+            var trav = node.Next;
+
+            node.Data = default;
+            node.Prev = null;
+            node.Next = null;
+
+            node = trav;
+
+        }
+
+        _head = _tail = null;
+        Count = 0;
+    }
+
+    public void AddLast(T item)
+    {
+        var newNode = new Node<T>(item, null, null);
+        if (this._head is null && this._tail is null)
+        {
+            this._head = this._tail = newNode;
+        }
+        else
+        {
+            _tail!.Next = newNode;
+            newNode.Prev = this._tail;
+
+            this._tail = newNode;
+        }
+        Count++;
+    }
+
+    public void AddFirst(T item)
+    {
+        var newNode = new Node<T>(item, null, null);
+
+        if (_head is null)
+        {
+            _tail = _head = newNode;
+        }
+        else
+        {
+            newNode.Next = this._head;
+            _head.Prev = newNode;
+
+            _head = newNode;
+        }
+
+        Count++;
+    }
+
+    public T? PeekFirst()
+    {
+        if (this.IsEmpty)
+            throw new InvalidOperationException("The list is empty");
+
+        return this._head!.Data;
+    }
+
+    public T? PeekLast()
+    {
+        if (this.IsEmpty)
+            throw new InvalidOperationException("The list is empty");
+
+        return this._tail!.Data;
+    }
+
+    public T? RemoveFirst()
+    {
+        if (this.IsEmpty)
+            throw new InvalidOperationException("The list is empty");
+
+        var node = this._head;
+        this._head = null;
+
+        this._head = node!.Next;
+        if (this._head is not null)
+            this._head!.Prev = null;
+
+        this.Count--;
+
+        if (IsEmpty)//when removing the first element the list is empty set the tail as null...
+            _tail = null;
+
+        return node.Data;
+    }
+
+    public T? RemoveLast()
+    {
+        if (this.IsEmpty)
+            throw new InvalidOperationException("The list is empty");
+
+        var node = this._tail;
+        this._tail = null;
+
+        this._tail = node!.Prev;
+        if (this._tail is not null)
+            this._tail!.Next = null;
+
+        this.Count--;
+
+        if (IsEmpty)//when removing the first element the list is empty set the tail as null...
+            _head = null;
+
+        return node.Data;
+    }
+
+    //NOTE: remove a node from the linked list in O(1)
+    private T? Remove(Node<T>? node)
+    {
+        if (node is null)
+            return default;
+
+        //is the head?
+        if (node!.Prev is null)
+            return RemoveFirst();
+        //is the tail?
+        if (node.Next is null)
+            return RemoveLast();
+
+        node.Next!.Prev = node.Prev;
+        node.Prev!.Next = node.Next;
+
+        var data = node.Data;
+
+        node.Prev = node.Next = null;
+
+        Count--;
+
+        return data;
+    }
+
+    public T? Remove(T? item)
+    {
+        var node = this._head;
+        while (node is not null
+            && node?.Data?.Equals(item) is false)
+        {
+            node = node.Next;
+        }
+
+        return Remove(node);
+    }
+
+    public T? RemoveAt(int index)
+    {
+        if (index < 0
+            || index >= Count) throw new ArgumentOutOfRangeException($"Index value {index} is out of range");
+
+        var acc = 0;
+        Node<T> node;
+        //while (node is not null 
+        //    && count <= index)
+        //{
+        //    node = node.Next;
+        //    count++;
+        //}
+
+        //with small clever improvement
+        if (Count / 2 >= index)
+        {//search in the left side of the list 
+            node = this._head;
+            while (node is not null
+                && acc != index)
+            {
+                node = node.Next;
+                acc++;
+            }
+        }
+        else
+        {//is probably on the right side of the list 
+            acc = Count - 1;
+            node = this._tail;
+            while (node is not null
+                && acc != index)
+            {
+                node = node.Prev;
+                acc--;
+            }
+        }
+
+        return Remove(node);
+
+    }
+
+    public int IndexOf(T? item)
+    {
+        if (IsEmpty)
+            return -1;
+
+        var node = this._head;
+        var acc = 0;
+        while (node is not null
+            && node.Data?.Equals(item) is false)
+        {
+            node = node.Next;
+            acc++;
+        }
+
+        if(node is null)
+            return -1;
+
+        return acc;
+    }
+
+    public bool Contains(T? item) =>
+        IndexOf(item) >= 0;
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var node = _head;
+        while (node is not null)
+        {
+            yield return node.Data!;
+            node = node.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
---
+++ BasicStructureTests/DoubledLinkedListTest.cs
@@ -0,0 +1,441 @@
+﻿using BasicStructurePractices;
+using FluentAssertions;
+
+namespace BasicStructureTests;
+
+[TestClass]
+public class DoubledLinkedListTest
+{
+    [TestMethod]
+    public void DLL_Add_Should_AddNewItem()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        l.Add("Oh");
+        l.Add("Bananas");
+        l.Add("!!!");
+
+        //ASSERT
+        l.Count.Should().Be(3);
+        l.Should().HaveCount(3);
+    }
+
+    [TestMethod]
+    public void DLL_Clear_Should_EmptyTheList()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>
+        {
+            "Oh",
+            "Bananas",
+            "!!!"
+        };
+
+        //ACT
+        l.Clear();
+
+        //ASSERT
+        l.Count.Should().Be(0);
+        l.Any().Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void DLL_IsEmpty_When_NoAddedItems_Should_Return_True()
+    {
+        //ARRANGE, ACT 
+        var l = new DoubleLinkedList<string>();
+
+        //ASSERT
+        l.Should().BeEmpty();
+        l.Count.Should().Be(0);
+        l.IsEmpty.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void DLL_AddLast_Should_AddAfterLastElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ASSERT
+        l.Count.Should().Be(3);
+        l.Should().HaveCount(3);
+        l.Last().Should().Be("!!!");
+        string.Join(" ", l.ToArray()).Should().Be("Oh Bananas !!!");
+    }
+
+    [TestMethod]
+    public void DLL_AddFirst_Should_AddBeforeFirstElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        l.AddFirst("Oh");
+        l.AddFirst("Bananas");
+        l.AddFirst("!!!");
+
+        //ASSERT
+        l.Count.Should().Be(3);
+        l.Should().HaveCount(3);
+        l.First().Should().Be("!!!");
+        string.Join(" ", l.ToArray()).Should().Be("!!! Bananas Oh");
+    }
+
+    [TestMethod]
+    public void DLL_PeekFirst_Should_Return_FirstElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.PeekFirst();
+
+        //ASSERT
+        l.Count.Should().Be(3);
+        l.Should().HaveCount(3);
+        uut.Should().Be("Oh");
+    }
+
+    [TestMethod]
+    public void DLL_PeekFirst_When_ListIsEmpty_Should_Throw_InvalidOperationException()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ASSERT, ACT
+        Action uut = () => l.PeekFirst();
+        uut.Should().Throw<InvalidOperationException>();
+        l.Count.Should().Be(0);
+        l.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public void DLL_PeekLast_Should_Return_LastElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.PeekLast();
+
+        //ASSERT
+        l.Count.Should().Be(3);
+        l.Should().HaveCount(3);
+        uut.Should().Be("!!!");
+    }
+
+    [TestMethod]
+    public void DLL_PeekLast_When_ListIsEmpty_Should_Throw_InvalidOperationException()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ASSERT, ACT
+        Action uut = () => l.PeekLast();
+        uut.Should().Throw<InvalidOperationException>();
+        l.Count.Should().Be(0);
+        l.Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public void DLL_RemoveFirst_Should_RemoveFirstElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.RemoveFirst();
+
+        //ASSERT
+        l.Count.Should().Be(2);
+        l.Should().HaveCount(2);
+        uut.Should().Be("Oh");
+    }
+
+    [TestMethod]
+    public void DLL_RemoveFirst_When_Empty_Should_Throw_InvalidOperationException()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT, ASSERT
+        Action uut = () => l.RemoveFirst();
+
+        l.Count.Should().Be(0);
+        l.Should().HaveCount(0);
+        uut.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestMethod]
+    public void DLL_RemoveFirst_When_ListHaveOneItem_Should_Set_TailToNull()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+
+        //ACT, ASSERT
+        var uut = l.RemoveFirst();
+
+        l.Count.Should().Be(0);
+        l.Should().HaveCount(0);
+        uut.Should().Be("Oh");
+    }
+
+    [TestMethod]
+    public void DLL_RemoveLast_Should_RemoveLastElement()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.RemoveLast();
+
+        //ASSERT
+        l.Count.Should().Be(2);
+        l.Should().HaveCount(2);
+        uut.Should().Be("!!!");
+    }
+
+    [TestMethod]
+    public void DLL_RemoveLast_When_Empty_Should_Throw_InvalidOperationException()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT, ASSERT
+        Action uut = () => l.RemoveLast();
+
+        l.Count.Should().Be(0);
+        l.Should().HaveCount(0);
+        uut.Should().Throw<InvalidOperationException>();
+    }
+
+    [TestMethod]
+    public void DLL_RemoveLast_When_ListHaveOneItem_Should_Set_TailToNull()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+
+        //ACT, ASSERT
+        var uut = l.RemoveLast();
+
+        l.Count.Should().Be(0);
+        l.Should().HaveCount(0);
+        uut.Should().Be("Oh");
+    }
+
+    [TestMethod]
+    public void DLL_Remove_When_DeleteFirstElement_Should_RemoveItem()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.Remove("Oh");
+
+        uut.Should().Be("Oh");
+        l.Count.Should().Be(2);
+        l.Count().Should().Be(2);
+    }
+
+    [TestMethod]
+    public void DLL_Remove_When_DeleteLastElement_Should_RemoveItem()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.Remove("!!!");
+
+        uut.Should().Be("!!!");
+        l.Count.Should().Be(2);
+        l.Count().Should().Be(2);
+    }
+
+    [TestMethod]
+    public void DLL_Remove_When_DeleteSomethingInTheMiddle_Should_RemoveItem()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.Remove("Bananas");
+
+        uut.Should().Be("Bananas");
+        l.Count.Should().Be(2);
+        l.Count().Should().Be(2);
+    }
+
+    [TestMethod]
+    public void DLL_Remove_When_EmptyList_Should_ReturnNull()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        var uut = l.Remove("Bananas");
+
+        uut.Should().BeNull();
+        l.IsEmpty.Should().BeTrue();
+        l.Any().Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void DLL_RemoveAt_When_GivingValidIndex_Should_RemoveItemAtGivenIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.RemoveAt(1);
+
+        uut.Should().Be("My");
+        l.Count.Should().Be(3);
+        l.Count().Should().Be(3);
+    }
+
+    [TestMethod]
+    public void DLL_RemoveAt_When_IndexIsAtTheEnd_Should_RemoveItemAtGivenIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Delicious");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.RemoveAt(3);
+
+        uut.Should().Be("Bananas");
+        l.Count.Should().Be(4);
+        l.Count().Should().Be(4);
+    }
+
+    [TestMethod]
+    public void DLL_IndexOf_Should_ReturnItemIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Delicious");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.IndexOf("Bananas");
+
+        uut.Should().Be(3);
+    }
+
+    [TestMethod]
+    public void DLL_IndexOf_When_EmptyList_Should_ReturnInvalidIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        var uut = l.IndexOf("Apple");
+
+        uut.Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void DLL_IndexOf_When_ItemNotExistInTheList_Should_ReturnInvalidIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Delicious");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.IndexOf("Apple");
+
+        uut.Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void DLL_Contains_Should_ReturnTrue()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Delicious");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.Contains("Bananas");
+
+        uut.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void DLL_Contains_When_EmptyList_Should_ReturnFalse()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+
+        //ACT
+        var uut = l.IndexOf("Bananas");
+
+        uut.Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void DLL_Contains_When_ItemNotExistInTheList_Should_ReturnInvalidIndex()
+    {
+        //ARRANGE
+        var l = new DoubleLinkedList<string>();
+        l.AddLast("Oh");
+        l.AddLast("My");
+        l.AddLast("Delicious");
+        l.AddLast("Bananas");
+        l.AddLast("!!!");
+
+        //ACT
+        var uut = l.Contains("Apple");
+
+        uut.Should().BeFalse();
+    }
+}
