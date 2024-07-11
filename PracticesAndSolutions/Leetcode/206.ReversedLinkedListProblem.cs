87ec8dafd6fe9fcb6d2416ad2b11dccb2542077f
lowseven <Lowseven@live.com>
2024-07-10 21:05:08 -0400

    Adding leet problems 206 and 22 - 206. Reversed Linked List - 22. Merge Two Sorted List

---
+++ PracticesAndSolutions/Leetcode/206.ReversedLinkedListProblem.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace PracticesAndSolutions.Leetcode;
+
+//https://leetcode.com/problems/reverse-linked-list/description/
+public class ReversedLinkedListProblem
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        //Follow up: A linked list can be reversed either iteratively or recursively.Could you implement both?
+
+        //-------------------------------------------
+        //recursive solution
+
+        public ListNode ReverseList(ListNode head)
+        {
+            return Reverse(head, null);
+        }
+
+        private ListNode Reverse(ListNode curr, ListNode prev)
+        {
+            if (curr == null)
+                return prev;
+
+            var next = curr.next;
+            curr.next = prev;
+
+            return Reverse(next, curr);
+        }
+
+
+
+        //-------------------------------------------
+        //iterative solution
+        //public ListNode Head;
+
+        //public ListNode ReverseList(ListNode head)
+        //{
+        //    var node = head;
+        //    while (node != null)
+        //    {
+
+        //        this.AddAtHead(node.val);
+        //        node = node.next;
+        //    }
+
+        //    return this.Head;
+        //}
+
+        //public void AddAtHead(int val)
+        //{
+        //    var newNode = new ListNode(val);
+        //    if (Head == null)
+        //        Head = newNode;
+        //    else
+        //    {
+        //        newNode.next = Head;
+        //        Head = newNode;
+        //    }
+        //}
+    }
+}
---
+++ PracticesAndSolutions/Leetcode/22.MergeTwoSortedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PracticesAndSolutions.Leetcode.ReversedLinkedListProblem;
+
+namespace PracticesAndSolutions.Leetcode;
+
+//https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedListProblem
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var dummyHead = new ListNode(-9999);
+        var curr = dummyHead;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val <= list2.val)
+            {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = list1 ?? list2;
+
+        return dummyHead.next;
+    }
+}
